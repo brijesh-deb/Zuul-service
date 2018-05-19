119deb704e0d4ee2b38fe6cca25a0079ec654106
Brijesh <212393369@logon.ds.ge.com>
2018-05-19 17:20:47 +0530

    Initial Commit

---
+++ src/main/java/com/java/brijesh/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,23 @@
+package com.java.brijesh.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.java.brijesh.gatewayservice.filters.PreFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	public PreFilter preFilter()
+	{
+		return new PreFilter();
+	}
+}
